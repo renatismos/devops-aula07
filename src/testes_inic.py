cd94b1870e49a28fb2eca968361728d1d14cc75e
Bruno Flaks <42192055+Flaks009@users.noreply.github.com>
2018-09-18 20:40:23 -0300

    Create testes_inic.py

---
+++ src/testes_inic.py
@@ -0,0 +1,26 @@
+import jogovelha
+
+import sys
+
+erroInicializar = False
+
+jogovelha.inicializar()
+jogo = jogovelha.tabuleiro()
+
+if len(jogo) != 3:
+  erroInicializar = True
+else:
+  for linha in jogo:
+    if len(linha) != 3:
+      erroInicializar = True
+    else:
+      for elemento in linha:
+        if elemento != '.':
+            erroInicializar = True
+            
+if erroInicializar:
+  print('Erro!')
+  sys.exit(1)
+
+else:
+  sys.exit(0)
